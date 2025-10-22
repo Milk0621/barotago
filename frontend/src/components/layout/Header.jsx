ed853faeb26f591e82006408f58130ff9d820a9d
Milk0621 <wltjs5360@gmail.com>
2025-10-23 01:41:17 +0900

    feat: 기본 레이아웃 구조 및 로고 이미지 추가

---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div style={{textAlign: 'center', padding: '44px 0'}}>
+            <img src="/img/logo.png" alt="barotago" />
+        </div>
+    )
+}
+
+export default Header;
