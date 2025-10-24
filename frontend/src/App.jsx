a19249991004b0efa2d875454bc52643f289dc49
Milk0621 <wltjs5360@gmail.com>
2025-10-24 10:32:06 +0900

    feat: AppRouter 구성 및 기본 라우팅 구조 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import AppRouter from './routes/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App wrap">
+      <AppRouter>
+
+      </AppRouter>
     </div>
   );
 }
