32112cbca215e8739b61fd06ba7b66490707cf20
Milk0621 <wltjs5360@gmail.com>
2025-10-28 14:04:54 +0900

    feat: 노선 선택 드롭다운 기본 구조 및 상태관리 구현

--- frontend/src/page/home/Home.jsx
+++ frontend/src/page/home/Home.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import "./Home.css";
+
 function Home() {
+    const [listOpen, setListOpen] = useState(false);    // 드롭다운 상태
+    const [line, setLine] = useState("1호선");          // 현재 호선
+
+    // 노선 목록
+    const subwayLines = [
+        "1호선",
+        "2호선",
+        "3호선",
+        "4호선",
+        "5호선",
+        "6호선",
+        "7호선",
+        "8호선",
+        "9호선",
+    ];
+
+    // 드롭다운
+
+
     return(
         <>
-        
+            <header className="dropdown">
+                <button className="dropdown-btn" onClick={()=>setListOpen(true)}>{line} 노선 <span className="dropdown-icon">▼</span></button>
+                {listOpen && (
+                    <ul className="dropdown-list">
+                        {subwayLines.map((lineName)=>(
+                            <li 
+                                key={lineName}
+                                className="dropdown-item"
+                                onClick={()=>{
+                                    setLine(lineName);
+                                    setListOpen(false);
+                                }}
+                            >
+                                {lineName}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </header>
+
+            <div className="line-container">
+                
+            </div>
         </>
     )
 }
