7c003a3334015c27c3e215297d7dc1c939105c23
Milk0621 <wltjs5360@gmail.com>
2025-11-06 23:47:18 +0900

    feat: 드롭다운 외부 클릭 시 자동으로 닫히는 기능 추가

--- frontend/src/page/home/Home.jsx
+++ frontend/src/page/home/Home.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Home.css";
 
 function Home() {
     const [listOpen, setListOpen] = useState(false);    // 드롭다운 상태
+    const dropdownRef = useRef(null);                   
     const [line, setLine] = useState("1호선");          // 현재 호선
 
     // 노선 목록
@@ -22,9 +23,26 @@ function Home() {
 
     // 노선에 따른 색상 지정
 
+    // 드롭다운 밖 클릭 감지 이벤트
+    useEffect(()=>{
+        if (!listOpen) return;
+
+        function handleClickOutside(e) {
+            // dropdown.current가 있고, 클릭한 대상이 그 영역 밖이라면 닫기
+            if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+                setListOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside); // 클릭 감지
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside); //cleanup
+        }
+    }, [])
+
     return(
         <>
-            <header className="dropdown">
+            <header className="dropdown" ref={dropdownRef}>
                 <button className="dropdown-btn" onClick={()=>setListOpen(true)}>{line} 노선 <span className="dropdown-icon">▼</span></button>
                 {listOpen && (
                     <ul className="dropdown-list">
