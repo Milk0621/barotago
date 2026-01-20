830cb87039e3e399e25df6be5550486e3be6cb2f
Milk0621 <wltjs5360@gmail.com>
2026-01-20 12:35:11 +0900

    feat: 컴포넌트 분리 및 CSS 수정

--- frontend/src/components/stationInfo/StationInfo.jsx
+++ frontend/src/components/stationInfo/StationInfo.jsx
@@ -1,14 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import facility_icon_map from "../../constants/facilityIcons";
 import "./StationInfo.css";
+
+const TAB = {
+    INFO: "INFO",
+    TIMETABLE: "TIMETABLE",
+};
 
 function StationInfo({stationInfo, facilities, onPrev, onNext, color, textColor}) {
     
     const mapRef = useRef();
-    
+    const [activeTab, setActiveTab] = useState(TAB.INFO);
+
     // 카카오 지도 생성
     useEffect(() => {
-        if (!stationInfo) return null;
+        if (activeTab !== TAB.INFO) return;
+        if (!stationInfo) return;
         if (!window.kakao || !window.kakao.maps) return;
 
         const kakao = window.kakao;
@@ -21,14 +28,13 @@ function StationInfo({stationInfo, facilities, onPrev, onNext, color, textColor}
         };
 
         new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
-    }, [stationInfo]);
+    }, [stationInfo, activeTab]);
+
+    if (!stationInfo) return null;
 
     return (
         <div className="station-modal" onClick={(e) => e.stopPropagation()} style={{ borderColor: color }}>
-            <div 
-                className="info-header"
-
-            >
+            <div className="info-header">
                 <span 
                     className="on-prev"                 
                     style={{
@@ -57,39 +63,22 @@ function StationInfo({stationInfo, facilities, onPrev, onNext, color, textColor}
                     ▶
                 </span>
             </div>
+            <div className="tab">
+                <button type="button" onClick={() => setActiveTab(TAB.INFO)}>역정보</button>
+                <button type="button" onClick={() => setActiveTab(TAB.TIMETABLE)}>열차시각표</button>
+            </div>
             <div className="tab-container">
-                <div className="tab">
-                    <button type="button">역정보</button>
-                    <button type="button">열차시각표</button>
-                </div>
-                <div className="tabBox1">
-                    <h3>역 정보</h3>
-                    <ul>
-                        <li>
-                            <strong>역주소</strong>
-                            <span>{stationInfo.address}</span>
-                        </li>
-                        <li>
-                            <strong>전화번호</strong>
-                            <span>{stationInfo.telno}</span>
-                        </li>
-                    </ul>
-                    <hr />
-                    <h3>편의시설</h3>
-                    <div className="facilities">
-                        {
-                            facilities.map((fc, idx)=>(
-                                <div className="facility" key={idx}>
-                                    <img src={facility_icon_map[fc.icon]} alt={fc.facilityNameKo} />
-                                    <p>{fc.facilityNameKo}</p>
-                                </div>
-                            ))
-                        }
-                    </div>
-                    <hr />
-                    <h3>지도</h3>
-                    <div id="map" ref={mapRef}></div>
-                </div>
+                {activeTab === TAB.INFO && (
+                    <InfoTab
+                        stationInfo={stationInfo}
+                        facilities={facilities}
+                        mapRef={mapRef}
+                    />
+                )}
+
+                {activeTab === TAB.TIMETABLE && (
+                    <TimetableTab />
+                )}
             </div>
         </div>
     )
@@ -97,3 +86,43 @@ function StationInfo({stationInfo, facilities, onPrev, onNext, color, textColor}
 
 export default StationInfo;
 
+function InfoTab ({stationInfo, facilities, mapRef}) {
+    return (
+        <div className="tabBox1">
+            <h3>역 정보</h3>
+            <ul>
+                <li>
+                    <strong>역주소</strong>
+                    <span>{stationInfo.address}</span>
+                </li>
+                <li>
+                    <strong>전화번호</strong>
+                    <span>{stationInfo.telno}</span>
+                </li>
+            </ul>
+            <div id="map" ref={mapRef}></div>
+            <hr />
+            <h3>편의시설</h3>
+            <div className="facilities">
+                {
+                    facilities.map((fc, idx)=>(
+                        <div className="facility" key={idx}>
+                            <img src={facility_icon_map[fc.icon]} alt={fc.facilityNameKo} />
+                            <p>{fc.facilityNameKo}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            <hr />
+            <h3>첫차/막차 시간표</h3>
+        </div>
+    )
+}
+
+function TimetableTab () {
+    return (
+        <div className="tabBox1">
+            <h3>열차 시각표</h3>
+        </div>
+    )
+}
